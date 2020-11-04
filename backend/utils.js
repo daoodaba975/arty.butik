c9818e06a04eb9b13e77dcc36edbcd077f016ac0
80-N <etienndour@gmail.com>
2020-11-04 22:03:53 +0100

    Shipping and order screens OK

--- backend/utils.js
+++ backend/utils.js
@@ -14,3 +14,24 @@ export const generateToken = (user) => {
     }
     );
 };
+export const isAuth = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (authorization) {
+      const token = authorization.slice(7, authorization.length); // Bearer XXXXXX
+      jwt.verify(
+        token,
+        // eslint-disable-next-line no-undef
+        process.env.JWT_SECRET || 'somethingsecret',
+        (err, decode) => {
+          if (err) {
+            res.status(401).send({ message: 'Authentification non valide' });
+          } else {
+            req.user = decode;
+            next();
+          }
+        }
+      );
+    } else {
+      res.status(401).send({ message: 'Authentification non valide' });
+    }
+  };
