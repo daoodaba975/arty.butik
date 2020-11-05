0fbeeb517e55302609dea07783883d5261fe1325
80-N <etienndour@gmail.com>
2020-11-05 22:21:56 +0100

    Orders admin implementing OK

--- backend/routers/orderRouter.js
+++ backend/routers/orderRouter.js
@@ -1,9 +1,18 @@
 import express from 'express';
 import expressAsyncHandler from 'express-async-handler';
 import Order from '../models/orderModel.js';
-import { isAuth } from '../utils.js';
+import { isAuth, isAdmin } from '../utils.js';
 
 const orderRouter = express.Router();
+orderRouter.get(
+  '/',
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const orders = await Order.find({}).populate('user', 'name');
+    res.send(orders);
+  })
+);
 orderRouter.get(
   '/mine', 
   isAuth, 
@@ -70,5 +79,40 @@ orderRouter.put(
     }
   })
 );
+
+orderRouter.delete(
+  '/:id',
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+      const deleteOrder = await order.remove();
+      res.send({ message: 'Commande supprimée', order: deleteOrder });
+    } else {
+      res.status(404).send({ message: 'Commande non trouvée' });
+    }
+  })
+);
+
+orderRouter.put(
+  '/:id/deliver',
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+      order.isDelivered = true;
+      order.deliveredAt = Date.now();
+
+      const updatedOrder = await order.save();
+      res.send({ message: 'Commande livrée', order: updatedOrder });
+    } else {
+      res.status(404).send({ message: 'Commande non trouvée' });
+    }
+  })
+);
+
+
 
 export default orderRouter;
